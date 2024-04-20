fd409f83380b9d40bffaef7473d8f615eb856d6c
Lavesh Akhadkar <117550850+Lavesh-Akhadkar@users.noreply.github.com>
2024-04-20 11:48:33 +0530

    Update api_config.py

--- api_config.py
+++ api_config.py
@@ -1,6 +1,5 @@
-import os
 
 # API configuration
 BINARY_API_URL = "https://api-inference.huggingface.co/models/parthsolanke/udot-forunf-mk1"
 MULTICLASS_API_URL = "https://api-inference.huggingface.co/models/parthsolanke/udot-mk2"
-API_TOKEN = os.environ.get("HF_API_TOKEN")
+API_TOKEN = "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"
